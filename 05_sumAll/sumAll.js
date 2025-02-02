8b01dc4ccff7f6636dca12588d663de5ff211fa9
tahis9 <147927771+tahis9@users.noreply.github.com>
2025-02-02 17:22:05 +1100

    Complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(...args) {
+  let result = 0;
+  for (let i = 0; i < args.length; i++) {
+    if (args[i] <= 0 || !Number.isInteger(args[i]) || typeof args[i] !== "number") {
+      return "ERROR";
+    }
+  }
+  if (args[0] <= args[1]) {
+    for (let i = args[0]; i <= args[1]; i++) {
+      result += i;
+    }
+  } else {
+    for (let i = args[1]; i <= args[0]; i++) {
+      result += i;
+    }
+  }
+  return result;
 };
 
 // Do not edit below this line
