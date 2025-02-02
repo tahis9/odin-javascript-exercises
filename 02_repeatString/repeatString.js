564e8fc66a9364a1df4960b9f5532e4c5934faba
tahis9 <147927771+tahis9@users.noreply.github.com>
2025-02-02 15:28:04 +1100

    Complete exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(string, times) {
+  result = '';
+  if (times < 0) {
+    result += "ERROR";
+  } else {;
+    for (let i = 1; i <= times; i++) {
+      result += string;
+    }
+  }
+  return result;
 };
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
