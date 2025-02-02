504b7ce8ad485438e1724d80703cc9aeae66490c
tahis9 <147927771+tahis9@users.noreply.github.com>
2025-02-02 16:43:41 +1100

    Complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+  let result = [];
+  outer: for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < args.length; j++) {
+      if (array[i] === args[j]) {
+        continue outer;
+      }
+    }
+    result.push(array[i]);
+  }
+  return result;
 };
 
 // Do not edit below this line
