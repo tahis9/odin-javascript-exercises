10dfd1fb85ff8362caedfb74c9679fc2422d4eeb
tahis9 <147927771+tahis9@users.noreply.github.com>
2025-02-02 17:42:26 +1100

    Complete exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fah) {
+  let cel = (fah-32) * 5 / 9;
+  return Math.round(cel*10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cel) {
+  let fah = (cel * 9 / 5 + 32);
+  return Math.round(fah*10) / 10;
 };
 
 // Do not edit below this line
