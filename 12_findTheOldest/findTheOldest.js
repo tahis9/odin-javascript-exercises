29888248ed9b4422dc2a3c63f676b3822f0583f0
tahis9 <147927771+tahis9@users.noreply.github.com>
2025-03-11 21:41:31 +1100

    Finish exercise 12 & all js exercises

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+  arr.forEach((obj) => {
+    if (!obj.yearOfDeath){
+      obj.yearOfDeath = (new Date()).getFullYear();
+    }
+  });
 
+  let sortedByAge = arr.sort((a,b)=>(b.yearOfDeath-b.yearOfBirth) - (a.yearOfDeath-a.yearOfBirth));
+  return sortedByAge[0]
 };
 
 // Do not edit below this line
