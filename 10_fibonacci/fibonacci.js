8cc89d6729fb5883d78c95133b1bc347db2735a5
tahis9 <147927771+tahis9@users.noreply.github.com>
2025-03-11 21:17:00 +1100

    Finish exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+  a = +a;
+  if (a < 0 || !Number.isInteger(a)){
+    return "OOPS"
+  }
+  let result = [0];
+  if (a > 0 && Number.isInteger(a)){
+    result[1] = 1;
+    for (let i = 2; i <= a; i++) {
+      result[i] = result[i-1] + result[i-2]
+    }
+  }
+  return result[a];
 };
 
 // Do not edit below this line
