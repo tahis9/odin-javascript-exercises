f23945171877918382ed0a21f7ac60b453388df9
tahis9 <147927771+tahis9@users.noreply.github.com>
2025-03-11 19:25:38 +1100

    Finish exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(...args) {
+	return args[0].reduce((sum, current) => sum + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(...args) {
+  return args[0].reduce((sum, current) => sum * current);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let result = 1;
+	for (let i = a; i > 1; i--){
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
