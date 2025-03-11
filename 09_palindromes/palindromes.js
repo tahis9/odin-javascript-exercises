57e890da9ecf3ba8f4b491ecf780b61d3059b0e0
tahis9 <147927771+tahis9@users.noreply.github.com>
2025-03-11 19:56:55 +1100

    Finish exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (input) {
+  inputFiltered = input.replace(/[^\p{L}\p{N}\s]/gu, '').replace(/\s/g,'').toLowerCase();
+  backwards = inputFiltered.split("").reverse().join("");
+  return inputFiltered === backwards;
 };
 
 // Do not edit below this line
